3e17d254aad1c458b758cf4caa5784eac768ba1c
Ricardo Rocha <rocha.porto@gmail.com>
2014-04-09 15:33:36 +1200

    added shuffle based target host selection in nova evacuate handler.

--- eventhandlers/nova_evacuate_vms.py
+++ eventhandlers/nova_evacuate_vms.py
@@ -21,11 +21,16 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-import sys
 import argparse
+import random
+import re
+import sys
+import syslog
 from keystoneclient.v2_0 import client as kclient
 from keystoneclient import exceptions
 from novaclient.v1_1 import client as nclient
+
+syslog.openlog('icinga-nova-evacuate', 0, syslog.LOG_USER)
 
 #
 # State ID values for HOST states in nagios
@@ -54,6 +59,8 @@ parser.add_argument('--insecure', action='store_true', default=False,
     help='Do not perform certificate validation')
 parser.add_argument('--unreachable-is-down', action='store_true', default=False,
     help='True if we trigger evacuate on node unreachable')
+parser.add_argument('--compute-host-regex', metavar='compute_host_regex', type=str,
+    default='.*comp.*', help='True if we trigger evacuate on node unreachable')
 parser.add_argument('compute_host', metavar='compute_host', type=str,
     help='Hostname of the compute node to evacuate')
 parser.add_argument('state', metavar='state', type=str,
@@ -69,32 +76,53 @@ if args.unreachable_is_down:
 
 # Is the state DOWN, and the state type HARD? Otherwise we do nothing for now...
 if args.state_type != 'HARD' and args.state not in down_states:
+  syslog.syslog("%s down, but probe not in HARD state yet (not running)" % args.compute_host)
   sys.exit(0)
 
+syslog.syslog("%s DOWN and probe in HARD state, evacuating VMs" % args.compute_host)
 # Get a nova client object (it takes care of keystone auth too)
 try:
   nova = nclient.Client(args.username, args.password, args.tenant,
       auth_url=args.auth_url, region_name=args.region_name,
       insecure=args.insecure)
 except Exception as e:
-  print "Failed to authenticate to keystone: %s" % str(e)
+  syslog.syslog(syslog.LOG_ERR, "Failed to authenticate to keystone: %s" % str(e))
   sys.exit(-1)
 
 # Check nova-compute is marked down for the compute host
 for binary in ['nova-compute']:
   service_state = nova.services.list(host=args.compute_host, binary=binary)
   if len(service_state) != 1:
-    print "Got more than one %s on host %s" % (binary, args.compute_host)
+    syslog.syslog(syslog.LOG_ERR, "Got more than one %s on host %s" % (binary, args.compute_host))
     sys.exit(-1)
   if service_state.pop().state != 'down':
-    print "Nagios says down, but %s is still up in %s when querying nova" % (
-        binary, args.compute_host)
+    syslog.syslog(syslog.LOG_ERR, "Nagios says down, but %s is still up in %s when querying nova" % (
+        binary, args.compute_host))
     sys.exit(-1)
 
 # List VMs associated to that compute node
 vms = nova.servers.list(search_opts={'host': args.compute_host})
 
 # Trigger evacuate for each of the VMs
-for vm in vms:
-  vm.evacuate('compute2.example.com', False)
+# TODO: we should be using the scheduler instead of only a random assign
+# https://blueprints.launchpad.net/nova/+spec/flexible-evacuate-scheduler
+current_host = None
+compute_regex = re.compile(args.compute_host_regex)
+try:
+  hosts = nova.hosts.list()
+except Exception as e:
+  syslog.syslog(syslog.LOG_ERR, "Failed to list hosts :: %s" % e)
+for host in hosts:
+  if host.host_name == args.compute_host:
+    current_host = host
+    hosts.remove(current_host)
+  elif compute_regex.match(host.host_name) is None:
+    hosts.remove(host)
 
+for vm in vms:
+  target = random.choice(hosts).host_name
+  syslog.syslog("Evacuating '%s' to compute host '%s'" % (vm.name, target))
+  try:
+    vm.evacuate(target, True)
+  except Exception as e:
+    syslog.syslog(syslog.LOG_ERR, "Failed to evacuate vm '%s' :: %s" % (vm.name, e))
